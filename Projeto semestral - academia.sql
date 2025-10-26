d0f224dc37698f3090d033dadb9d98fa56cf9639
victor matheus <144499278+Mindvictor@users.noreply.github.com>
2025-10-26 00:43:00 -0300

    Add files via upload

---
+++ Projeto semestral - academia.sql
@@ -0,0 +1,31 @@
+/*CREATE DATABASE Academia;*/
+USE Academia;
+/*CREATE TABLE tbl_alunos (
+id_aluno INT AUTO_INCREMENT PRIMARY KEY,
+nome_aluno VARCHAR (100) NOT NULL,
+idade_aluno INT,
+telefone_aluno VARCHAR (30)
+);*/
+
+/*CREATE TABLE tbl_treinos (
+id_treino INT AUTO_INCREMENT PRIMARY KEY,
+nome_treino VARCHAR (50),
+data_treino DATE,
+duracao_treino INT,
+intensidade_treino VARCHAR (20)
+);*/
+
+/*INSERT INTO tbl_alunos (nome_aluno, idade_aluno, telefone_aluno) VALUES
+('Luiz Dias', 25, '(11) 94582-7319'),
+('Renato Batarci', 22, '(11) 97635-4291'),
+('Altamir Rodrigues', 40, '(11) 92148-5967')*/
+
+/*INSERT INTO tbl_treinos (nome_treino, data_treino, duracao_treino, intensidade_treino) VALUES
+('Treino de pernas', '2025-10-31', 55,'Moderada'),
+('Treino de peito', '2025-11-25', 45,'Alta'),
+('Treino de costas', '2025-08-10', 40, 'Alta'),
+('Treino de ombro', '2025-07-05', 35, 'Baixa')*/
+
+SELECT * FROM tbl_treinos
+
+
